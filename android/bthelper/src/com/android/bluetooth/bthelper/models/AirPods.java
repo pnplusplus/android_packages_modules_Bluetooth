pnplusplus/android_packages_modules_Bluetooth

commit bb2f5f5bf3f7d440f1a250e573a8900904bd5835

BtHelper: Apply rightLeft check to usingLeft/usingRight
 - Since we fixed right left pod status reporting shift detection logic. Due to this, usingLeft/usingRight's shifted report doesn't applied.

Fixes : b2e8641 ("BtHelper: Initial support for showing battery separately")
Change-Id: I958f550930fb49d89308dcfc543d5518d2e56f71

android/bthelper/src/com/android/bluetooth/bthelper/models/AirPods.java
@@ -86,8 +86,8 @@ public class AirPods {
         batteryCase = charging & 0xf;
         chargingCase = (charging & MASK_CHARGING_CASE) != 0;
 
-        usingLeft = (flags & MASK_USING_LEFT) != 0;
-        usingRight = (flags & MASK_USING_RIGHT) != 0;
+        usingLeft = (flags & (rightLeft == true ? MASK_USING_LEFT : MASK_USING_RIGHT)) != 0;
+        usingRight = (flags & (rightLeft == true ? MASK_USING_RIGHT : MASK_USING_LEFT)) != 0;
 
         if (chargingLeft == true
             && chargingRight == true) {

android/bthelper/src/com/android/bluetooth/bthelper/models/AirPodsGen2.java
@@ -86,8 +86,8 @@ public class AirPodsGen2 {
         batteryCase = charging & 0xf;
         chargingCase = (charging & MASK_CHARGING_CASE) != 0;
 
-        usingLeft = (flags & MASK_USING_LEFT) != 0;
-        usingRight = (flags & MASK_USING_RIGHT) != 0;
+        usingLeft = (flags & (rightLeft == true ? MASK_USING_LEFT : MASK_USING_RIGHT)) != 0;
+        usingRight = (flags & (rightLeft == true ? MASK_USING_RIGHT : MASK_USING_LEFT)) != 0;
 
         if (chargingLeft == true
             && chargingRight == true) {

android/bthelper/src/com/android/bluetooth/bthelper/models/AirPodsGen3.java
@@ -86,8 +86,8 @@ public class AirPodsGen3 {
         batteryCase = charging & 0xf;
         chargingCase = (charging & MASK_CHARGING_CASE) != 0;
 
-        usingLeft = (flags & MASK_USING_LEFT) != 0;
-        usingRight = (flags & MASK_USING_RIGHT) != 0;
+        usingLeft = (flags & (rightLeft == true ? MASK_USING_LEFT : MASK_USING_RIGHT)) != 0;
+        usingRight = (flags & (rightLeft == true ? MASK_USING_RIGHT : MASK_USING_LEFT)) != 0;
 
         if (chargingLeft == true
             && chargingRight == true) {

android/bthelper/src/com/android/bluetooth/bthelper/models/AirPodsPro.java
@@ -86,8 +86,8 @@ public class AirPodsPro {
         batteryCase = charging & 0xf;
         chargingCase = (charging & MASK_CHARGING_CASE) != 0;
 
-        usingLeft = (flags & MASK_USING_LEFT) != 0;
-        usingRight = (flags & MASK_USING_RIGHT) != 0;
+        usingLeft = (flags & (rightLeft == true ? MASK_USING_LEFT : MASK_USING_RIGHT)) != 0;
+        usingRight = (flags & (rightLeft == true ? MASK_USING_RIGHT : MASK_USING_LEFT)) != 0;
 
         if (chargingLeft == true
             && chargingRight == true) {

android/bthelper/src/com/android/bluetooth/bthelper/models/AirPodsProGen2.java
@@ -86,8 +86,8 @@ public class AirPodsProGen2 {
         batteryCase = charging & 0xf;
         chargingCase = (charging & MASK_CHARGING_CASE) != 0;
 
-        usingLeft = (flags & MASK_USING_LEFT) != 0;
-        usingRight = (flags & MASK_USING_RIGHT) != 0;
+        usingLeft = (flags & (rightLeft == true ? MASK_USING_LEFT : MASK_USING_RIGHT)) != 0;
+        usingRight = (flags & (rightLeft == true ? MASK_USING_RIGHT : MASK_USING_LEFT)) != 0;
 
         if (chargingLeft == true
             && chargingRight == true) {

commit 66d14c2c28d619726187fa95e36ddca555e926e8

fixup! ccb7dc3 BtHelper: Apply rightLeft check to usingLeft/usingRight
- Keep in sync with upstream base.

Fixes: bb2f5f5 ("BtHelper: Apply rightLeft check to usingLeft/usingRight")
CC: someone5678 <nemui3353@gmail.com>
Change-Id: I54c776fb521fa5fc989a19cac771eb7b06123b04
Signed-off-by: kawaaii <kawaaii@nocturn9x.space>

android/bthelper/src/com/android/bluetooth/bthelper/models/AirPods.java
@@ -86,8 +86,8 @@ public class AirPods {
         batteryCase = charging & 0xf;
         chargingCase = (charging & MASK_CHARGING_CASE) != 0;
 
-        usingLeft = (flags & (rightLeft == true ? MASK_USING_LEFT : MASK_USING_RIGHT)) != 0;
-        usingRight = (flags & (rightLeft == true ? MASK_USING_RIGHT : MASK_USING_LEFT)) != 0;
+        usingLeft = (flags & (rightLeft == true ? MASK_USING_RIGHT : MASK_USING_LEFT)) != 0;
+        usingRight = (flags & (rightLeft == true ? MASK_USING_LEFT : MASK_USING_RIGHT)) != 0;
 
         if (chargingLeft == true
             && chargingRight == true) {

android/bthelper/src/com/android/bluetooth/bthelper/models/AirPodsGen2.java
@@ -86,8 +86,8 @@ public class AirPodsGen2 {
         batteryCase = charging & 0xf;
         chargingCase = (charging & MASK_CHARGING_CASE) != 0;
 
-        usingLeft = (flags & (rightLeft == true ? MASK_USING_LEFT : MASK_USING_RIGHT)) != 0;
-        usingRight = (flags & (rightLeft == true ? MASK_USING_RIGHT : MASK_USING_LEFT)) != 0;
+        usingLeft = (flags & (rightLeft == true ? MASK_USING_RIGHT : MASK_USING_LEFT)) != 0;
+        usingRight = (flags & (rightLeft == true ? MASK_USING_LEFT : MASK_USING_RIGHT)) != 0;
 
         if (chargingLeft == true
             && chargingRight == true) {

android/bthelper/src/com/android/bluetooth/bthelper/models/AirPodsGen3.java
@@ -86,8 +86,8 @@ public class AirPodsGen3 {
         batteryCase = charging & 0xf;
         chargingCase = (charging & MASK_CHARGING_CASE) != 0;
 
-        usingLeft = (flags & (rightLeft == true ? MASK_USING_LEFT : MASK_USING_RIGHT)) != 0;
-        usingRight = (flags & (rightLeft == true ? MASK_USING_RIGHT : MASK_USING_LEFT)) != 0;
+        usingLeft = (flags & (rightLeft == true ? MASK_USING_RIGHT : MASK_USING_LEFT)) != 0;
+        usingRight = (flags & (rightLeft == true ? MASK_USING_LEFT : MASK_USING_RIGHT)) != 0;
 
         if (chargingLeft == true
             && chargingRight == true) {

android/bthelper/src/com/android/bluetooth/bthelper/models/AirPodsPro.java
@@ -86,8 +86,8 @@ public class AirPodsPro {
         batteryCase = charging & 0xf;
         chargingCase = (charging & MASK_CHARGING_CASE) != 0;
 
-        usingLeft = (flags & (rightLeft == true ? MASK_USING_LEFT : MASK_USING_RIGHT)) != 0;
-        usingRight = (flags & (rightLeft == true ? MASK_USING_RIGHT : MASK_USING_LEFT)) != 0;
+        usingLeft = (flags & (rightLeft == true ? MASK_USING_RIGHT : MASK_USING_LEFT)) != 0;
+        usingRight = (flags & (rightLeft == true ? MASK_USING_LEFT : MASK_USING_RIGHT)) != 0;
 
         if (chargingLeft == true
             && chargingRight == true) {

android/bthelper/src/com/android/bluetooth/bthelper/models/AirPodsProGen2.java
@@ -86,8 +86,8 @@ public class AirPodsProGen2 {
         batteryCase = charging & 0xf;
         chargingCase = (charging & MASK_CHARGING_CASE) != 0;
 
-        usingLeft = (flags & (rightLeft == true ? MASK_USING_LEFT : MASK_USING_RIGHT)) != 0;
-        usingRight = (flags & (rightLeft == true ? MASK_USING_RIGHT : MASK_USING_LEFT)) != 0;
+        usingLeft = (flags & (rightLeft == true ? MASK_USING_RIGHT : MASK_USING_LEFT)) != 0;
+        usingRight = (flags & (rightLeft == true ? MASK_USING_LEFT : MASK_USING_RIGHT)) != 0;
 
         if (chargingLeft == true
             && chargingRight == true) {